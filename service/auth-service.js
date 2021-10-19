91d716f82b0f1ff2b9d42e2fbb8b88d919810616
daniel0426 <angus9209@gmail.com>
2021-10-20 07:56:47 +1300

    firebase setup complete

---
+++ service/auth-service.js
@@ -0,0 +1,43 @@
+import { 
+    getAuth, 
+    signInWithPopup, 
+    GoogleAuthProvider,
+    GithubAuthProvider,
+    onAuthStateChanged,
+    signOut, 
+} from "firebase/auth";
+
+
+class AuthService{
+    constructor(){
+        this.firebaseAuth = getAuth();
+        this.googleProvider = new GoogleAuthProvider();
+        this.githubProvider = new GithubAuthProvider();
+    }
+
+    login(providerName){
+        const authProvider = this.getProvider(providerName)
+        return signInWithPopup(this.firebaseAuth, authProvider)
+    }
+
+    logout(){
+        this.firebaseAuth.signOut();
+    }
+
+    onAuthChange(onUserChanged) {
+        this.firebaseAuth.onAuthStateChanged(user => onUserChanged(user) )
+    }
+
+    getProvider(providerName){
+        switch(providerName){
+            case 'google' :
+                return this.googleProvider;
+            case 'github' :
+                return this.githubProvider;
+            default : 
+            throw new Error(`not supported provider : ${providerName} `)
+        }
+    }
+}
+
+export default AuthService
