efc43a59d35a7d0fc71885053713caa01827b1dd
daniel0426 <angus9209@gmail.com>
2021-10-25 18:03:55 +1300

    finding the error for deploy build production

--- src/app.jsx
+++ src/app.jsx
@@ -6,16 +6,16 @@ import Maker from './components/maker/maker';
 function App({FileInput, authService, cardRepository}) {
   return (
     <BrowserRouter>
-    <div className={styles.app}>
-      <Switch>
-        <Route exact path='/'>
-          <Login authService = {authService}/>
-        </Route>
-        <Route path='/main'>
-            <Maker cardRepository={cardRepository} FileInput = {FileInput} authService={authService}/>
-        </Route>
-      </Switch>
-    </div>
+      <div className={styles.app}>
+        <Switch>
+          <Route exact path='/'>
+            <Login authService = {authService}/>
+          </Route>
+          <Route path='/main'>
+              <Maker cardRepository={cardRepository} FileInput = {FileInput} authService={authService}/>
+          </Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   )
 }
